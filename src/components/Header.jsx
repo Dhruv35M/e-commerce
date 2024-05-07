6bdf638e1593dffd49c7c70f7e030de3666c82aa
dhruv mishra <dhruv35m@gmail.com>
2024-05-08 02:27:42 +0530

    project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="h-16 shadow-md bg-white fixed w-full z-40">
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+        <div className="">
+          <Logo />
+        </div>
+
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
+          <input
+            type="text"
+            placeholder="search product ..."
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-blue-500 flex items-center justify-center rounded-r-full text-white cursor-pointer">
+            <GrSearch />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-7">
+          {/* <div className="relative flex justify-center"> */}
+          <div className="text-3xl cursor-pointer relative flex justify-center">
+            <FaRegCircleUser />
+          </div>
+          <div className="text-2xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+            <div className="bg-blue-500 text-  w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+            {/* </div> */}
+          </div>
+
+          <div>
+            <button className="px-3 py-1 rounded-full text-white bg-blue-500  hover:bg-blue-700">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src="https://dhruvmishra.com/logo.png" width={150} height={150} />
+    </div>
+  );
+};
+
+export default Logo;
